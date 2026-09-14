52940a2a039542c3351eeb83556de035fd5921bf
Jordan Siaw <jordan.siaw@redis.com>
2026-09-14 14:32:50 -0700

    nvim: Pin monokai-pro to v2.1.1 to avoid bug

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,10 @@
 return {
-  { "loctvl842/monokai-pro.nvim" },
+  {
+    "loctvl842/monokai-pro.nvim",
+    -- Pin to this version for now to avoid:
+    -- https://github.com/loctvl842/monokai-pro.nvim/issues/173#issuecomment-4640411811
+    version = "v2.1.1",
+  },
   {
     "LazyVim/LazyVim",
     opts = {
