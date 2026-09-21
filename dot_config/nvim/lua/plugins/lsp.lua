b630cb1d4e2fae9e9a0414b24b256a3d182dbb1d
Jordan Siaw <jordan.siaw@redis.com>
2026-09-21 15:39:25 -0700

    nvim: Disable inlay hints by default

---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        -- Hints make the code hard to read. It can
+        -- always be toggled on if needed.
+        enabled = false,
+      },
+    },
+  },
+}
