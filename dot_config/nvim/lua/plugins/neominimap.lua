32167ac4b9af9631f3c4471ff9ceab4674fb1c0c
Jordan Siaw <jordan.siaw@redis.com>
2026-09-22 11:01:04 -0700

    nvim: Compress minimap vertically, update it less, make it scroll less

--- dot_config/nvim/lua/plugins/neominimap.lua
+++ dot_config/nvim/lua/plugins/neominimap.lua
@@ -3,11 +3,23 @@ return {
   version = "v3.x.x",
   lazy = false,
   init = function()
-    -- Use 'split' instead of 'float' so that the minimap
-    -- doesn't hide content in the editing buffer.
     vim.g.neominimap = {
       auto_enable = true,
+
+      -- Use 'split' instead of 'float' so that the minimap
+      -- doesn't hide content in the editing buffer.
       layout = "split",
+
+      -- Scroll the minimap marker instead of scrolling
+      -- the minimap around the marker.
+      current_line_position = "percent",
+
+      -- Compress the minimap vertically.
+      y_multiplier = 2.5,
+
+      -- Update the minimap less frequently (in ms).
+      delay = 5000,
+
       split = {
         minimap_width = 20,
         fix_width = true,
